9db4173ccde9c0cde75b7806d65ad74313414ea5
annsshadow <cravenboy@163.com>
2016-09-15 17:55:26 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Please enter two number:" << std::endl;
+	int num1 = 0, num2 = 0;
+	std::cin >> num1 >> num2;
+	std::cout << "sum of " << num1 << " and " << num2 << " is " << num1 + num2 << std::endl;
+	return 0;
+}
