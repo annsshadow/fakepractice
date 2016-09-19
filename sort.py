e2ac4b80bb14b22e18264c45be51be3f1828a18e
annsshadow <cravenboy@163.com>
2016-09-19 16:54:02 +0800

    sort

---
+++ sort.py
@@ -0,0 +1,34 @@
+def insert_sort(lists):
+    count = len(lists)
+
+    for i in range(1, count):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j + 1] = lists[j]
+                lists[j] = key
+            j -= 1
+    return lists
+
+
+def shell_sort(lists):
+    count = len(lists)
+    step = 2
+    group = count / step
+
+    while group > 0:
+        for i in range(0, group):
+            j = i + group
+            while j < count:
+                k = j - group
+                key = lists[j]
+                while k >= 0:
+                    if lists[k] > key:
+                        lists[k + group] = lists[k]
+                        lists[k] = key
+                    k -= group
+                j += group
+        group /= step
+
+    return lists
