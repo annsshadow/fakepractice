755e42a58e0f29e06c44922ce9bd60877cb69700
annsshadow <cravenboy@163.com>
2016-09-21 11:15:26 +0800

    sort

--- sort.py
+++ sort.py
@@ -1,3 +1,25 @@
+def bubble_sort(lists):
+    count = len(lists)
+
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+
+    return lists
+
+def select_sort(lists):
+    count = len(lists)
+
+    for i in range(0, count):
+        min = i
+        for j in range(i + 1, count):
+            if lists[min] > lists[j]:
+                min = j
+        lists[min], lists[i] = lists[i], lists[min]
+
+    return lists
+
 def insert_sort(lists):
     count = len(lists)
 
@@ -10,7 +32,6 @@ def insert_sort(lists):
                 lists[j] = key
             j -= 1
     return lists
-
 
 def shell_sort(lists):
     count = len(lists)
@@ -33,16 +54,6 @@ def shell_sort(lists):
 
     return lists
 
-def bubble_sort(lists):
-    count = len(lists)
-
-    for i in range(0, count):
-        for j in range(i + 1, count):
-            if lists[i] > lists[j]:
-                lists[i], lists[j] = lists[j], lists[i]
-
-    return lists
-
 def quick_sort(lists, left, right):
     if left >= right:
         return lists
@@ -62,5 +73,84 @@ def quick_sort(lists, left, right):
 
     quick_sort(lists, low, left - 1)
     quick_sort(lists, left + 1, high)
+
+    return lists
+
+def adjust_heap(lists, i, size):
+
+    lchild = 2 * i + 1
+    rchild = 2 * i + 2
+    max = i
+
+    if i < size / 2:
+        if lchild < size and lists[lchild] > lists[max]:
+            max = lchild
+        if rchild < size and lists[rchild] > lists[max]:
+            max = rchild
+        if max != i:
+            lists[max], lists[i] = lists[i], lists[max]
+            adjust_heap(lists, max, size)
+
+ 
+
+def build_heap(lists, size):
+    for i in range(0, (size/2))[::-1]:
+        adjust_heap(lists, i, size)
+
+ 
+
+def heap_sort(lists):
+    size = len(lists)
+    build_heap(lists, size)
+
+    for i in range(0, size)[::-1]:
+        lists[0], lists[i] = lists[i], lists[0]
+        adjust_heap(lists, 0, i)
+
+def merge(left, right):
+
+     i, j = 0, 0
+    result = []
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+ 
+
+def merge_sort(lists):
+
+    if len(lists) <= 1:
+        return lists
+
+    num = len(lists) / 2
+    left = merge_sort(lists[:num])
+    right = merge_sort(lists[num:])
+
+    return merge(left, right)
+
+import math
+
+def radix_sort(lists, radix=10):
+
+    k = int(math.ceil(math.log(max(lists), radix)))
+    bucket = [[] for i in range(radix)]
+
+    for i in range(1, k+1):
+        for j in lists:
+            bucket[j/(radix**(i-1)) % (radix**i)].append(j)
+        del lists[:]
+        for z in bucket:
+            lists += z
+            del z[:]
 
     return lists
