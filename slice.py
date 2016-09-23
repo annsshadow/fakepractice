0db5febd6affed9108ad79fd7319d7176fa4d5ab
annsshadow <cravenboy@163.com>
2016-09-23 16:27:50 +0800

    slice+generator

---
+++ slice.py
@@ -0,0 +1,23 @@
+L = [1,2,3,4,5,6,7,8,9]
+print(L[::1])
+print(L[:8:1])
+print(L[-2::1])
+print(L[-2::-1])
+print(L[-2:0:-1])
+print(L[-2:0:-2])
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield(3)
+    print('step 3')
+    yield(5)
