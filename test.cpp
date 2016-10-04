1f9e72e9cd0da619b3ab9c4eb914e7d9af2e0dc4
annsshadow <cravenboy@163.com>
2016-10-04 17:24:55 +0800

    cl

--- test.cpp
+++ test.cpp
@@ -5,5 +5,24 @@ int main(){
 	int num1 = 0, num2 = 0;
 	std::cin >> num1 >> num2;
 	std::cout << "sum of " << num1 << " and " << num2 << " is " << num1 + num2 << std::endl;
+
+	int sum = 0, value = 0,currentValue = 0;
+	if(std::cin >> currentValue){
+		int count = 1;
+		while(std::cin >> value){
+			if(value == currentValue){
+				count++;
+			}
+			else{
+				std::cout << "currentValue is : " << currentValue << " occurs " << count << " times " << std::endl;
+				currentValue = value;
+				count = 1;
+			}
+			sum += value;
+		}
+	}
+	
+	std::cout << "sum is : " << sum << std::endl;
+
 	return 0;
 }
