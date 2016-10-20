68a29cd3e2b65a02ebcbe8976e188a13fa30b319
shadowwen-PC0\Administrator <cravenboy@163.com>
2016-10-20 21:48:41 +0800

    test

---
+++ c-cpp/test_data_type.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("char:%d\n",sizeof(char));
+    printf("int:%d\n",sizeof(int));
+    printf("long int:%ld\n",sizeof(long));
+    printf("float:%d\n",sizeof(float));
+    printf("double:%d\n",sizeof(double));
+
+    system("pause");
+    return 0;
+}
