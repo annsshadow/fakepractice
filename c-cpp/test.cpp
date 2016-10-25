fb09cb73dc5c9bb7194c2c93796adb78539efff8
shadowwen-PC0\Administrator <cravenboy@163.com>
2016-10-25 18:38:04 +0800

    update gitignore

--- c-cpp/test.cpp
+++ c-cpp/test.cpp
@@ -1,9 +1,10 @@
+#include <boost/regex.hpp>
 #include <iostream>
-
+#include <string>
 using namespace std;
-
 int main()
 {
+    /*
     cout << "Please enter two number:" << endl;
     int num1 = 0, num2 = 0;
     //cin >> num1 >> num2;
@@ -33,5 +34,15 @@ int main()
 
     cout << "sum is : " << sum << endl;
 
+    return 0;
+    */
+    /*用法一：
+    *用str替换指定字符串从起始位置pos开始长度为len的字符
+    *string& replace (size_t pos, size_t len, const string& str);
+    */
+
+    string line = "test;testtt;jjjjj;";
+    line = line.replace(line.find(";"), line.length(), "\n"); //从第一个@位置替换第一个@为空
+    cout << line << endl;
     return 0;
 }
